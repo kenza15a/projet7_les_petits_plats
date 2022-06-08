3c0e03d3266c057bd52cf3cb78426ade4d2d83f9
Kenza Filali <filali1991.univ@gmail.com>
2022-06-08 16:32:57 +0200

    styles cards ingredients

--- src/factories/ingredientsFactory.js
+++ src/factories/ingredientsFactory.js
@@ -5,14 +5,15 @@ export default class ingredientsFactory {
         this._ingredientsTab = ingredientsTab;
 
     }
+    /*lister les ingredients d'une recette*/
     getIngredientsLi() {
 
         const { ingredient, quantity, unit } = this._ingredientsTab;
         let liIngredients = document.createElement('li');
         if ('unit' in this._ingredientsTab) {
-            liIngredients.innerHTML = `<strong>${ingredient}: </strong> ${quantity}${unit}`;
+            liIngredients.innerHTML = `<strong>${ingredient}: </strong><span>  ${quantity}${unit}</span>`;
         } else {
-            liIngredients.innerHTML = `<strong>${ingredient}: </strong> ${quantity}`;
+            liIngredients.innerHTML = `<strong>${ingredient}: </strong>  <span>${quantity}</span>`;
 
         }
 
